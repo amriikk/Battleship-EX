e6a71a4074c6e971be7fbc8ab077e20031e8d1c2
John Khrizhtian Trujillo <yiannix@johns-mbp.home>
2020-03-27 01:53:41 -0700

    set up rules modal

--- js/main.js
+++ js/main.js
@@ -33,7 +33,6 @@ const model = {
                         setTimeout(()=> {
                             this.reset()
                          }, 1500);
-                         location.reload;
                         init();
                     }
                 }
@@ -128,7 +127,7 @@ const view = {
         cell.setAttribute("class", "miss");
     },
     displayReset: function(location){
-        // grab all element nodes from board and then delete
+        // grab all element nodes from board
         $('td').attr('class', "")
     }
 };
@@ -187,6 +186,35 @@ function handlekeyPress(e){
         return false;
     }
 };
+
+
+
+// Setting up Modal for Game Rules //
+
+
+// Get the modal
+    let modal = document.getElementById("myModal");
+// Get the button that opens the modal
+    let btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+// End of IceBox features
 
 function init() {
     let fireButton = document.getElementById("fireButton");
